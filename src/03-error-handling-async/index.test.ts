2fde19d29a1671dda985eb2302aaf5c9234a2081
KirillDemyanenko <demyanenkokg@gmail.com>
2023-06-28 20:18:55 +0300

    feat: Table tests

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,48 @@
-// Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
-
+import {
+  throwError,
+  throwCustomError,
+  resolveValue,
+  MyAwesomeError,
+  rejectCustomError,
+} from './index';
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
-    // Write your test here
-  });
+    expect(await resolveValue(10)).toBe(10);
+    expect(await resolveValue('10')).toBe('10');
+    expect(await resolveValue(null)).toBe(null);
+    expect(await resolveValue([1, 2, 3, 4, 5])).toStrictEqual([1, 2, 3, 4, 5]);
+    expect(await resolveValue({ a: 1, b: 2, c: 3, d: 4, e: 5 })).toStrictEqual({
+      a: 1,
+      b: 2,
+      c: 3,
+      d: 4,
+      e: 5,
+    });
+  }, 30000);
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
-    // Write your test here
-  });
+    expect(() => throwError('error')).toThrow('error');
+    expect(() => throwError('msg')).toThrow('msg');
+    expect(() => throwError('cat')).toThrow('cat');
+    expect(() => throwError('no error')).toThrow('no error');
+    expect(() => throwError('meow')).toThrow('meow');
+  }, 25000);
 
   test('should throw error with default message if message is not provided', () => {
-    // Write your test here
-  });
+    expect(() => throwError()).toThrow('Oops!');
+  }, 5000);
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
-    // Write your test here
-  });
+    expect(() => throwCustomError()).toThrow(MyAwesomeError);
+  }, 30000);
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
-    // Write your test here
-  });
+    await expect(rejectCustomError()).rejects.toThrowError(MyAwesomeError);
+  }, 30000);
 });
