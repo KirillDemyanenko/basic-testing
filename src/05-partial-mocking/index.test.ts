de971c8c47b9b8348051c426708c8af23165cf06
KirillDemyanenko <demyanenkokg@gmail.com>
2023-06-29 16:26:59 +0300

    feat: Partial mocking

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,8 +1,15 @@
-// Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  const originalModule =
+    jest.requireActual<typeof import('./index')>('./index');
+  return {
+    __esModule: true,
+    ...originalModule,
+    mockOne: jest.fn(),
+    mockTwo: jest.fn(),
+    mockThree: jest.fn(),
+  };
 });
 
 describe('partial mocking', () => {
@@ -11,10 +18,14 @@ describe('partial mocking', () => {
   });
 
   test('mockOne, mockTwo, mockThree should not log into console', () => {
-    // Write your test here
-  });
+    expect(mockOne).toHaveBeenCalledTimes(0);
+    expect(mockTwo).toHaveBeenCalledTimes(0);
+    expect(mockThree).toHaveBeenCalledTimes(0);
+  }, 30000);
 
   test('unmockedFunction should log into console', () => {
-    // Write your test here
-  });
+    const spy = jest.spyOn(console, 'log');
+    unmockedFunction();
+    expect(spy).toBeCalledWith('I am not mocked');
+  }, 30000);
 });
